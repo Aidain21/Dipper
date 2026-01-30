8afa9a89ed9c88c8e6a8fe99089e51e048577a19
Aidan Ainsley <aidanainsley@gmail.com>
2026-01-30 11:32:39 -0500

    Movement of the logo

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,6 +1,8 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,7 +12,7 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
-    public float testy = 0f;
+    public float testy = 0f, testy2 = 0f;
 
     @Override
     public void create() {
@@ -22,16 +24,26 @@ public class Main extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140 + testy, 210);
+        batch.draw(image, 140 + testy, 210 + testy2);
         batch.end();
-        testy += 0.1f;
         input();
         logic();
         draw();
     }
 
     private void input() {
-
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            testy -= 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            testy += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            testy2 += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            testy2 -= 1;
+        }
     }
 
     private void logic() {
