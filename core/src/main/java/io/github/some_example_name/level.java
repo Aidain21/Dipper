03d71ef8c5b4d63fddb637b3af07f7c1231e282d
Alex Dabrowski <dabrowa@sunypoly.edu>
2026-02-02 11:33:12 -0500

    cleaned up level file

--- core/src/main/java/io/github/some_example_name/level.java
+++ core/src/main/java/io/github/some_example_name/level.java
@@ -11,20 +11,11 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class level extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
     char[][] level1;
-    Texture bucketTexture;
-    Texture dropTexture;
-    SpriteBatch spriteBatch;
-    FitViewport viewport;
     int rowCount;
     int colCount;
-    boolean run = true;
-    int count = 0;
 
     public level(int r, int c){
-        //char[][] level1=new char[length][width];
         level1=new char[r][c];
         rowCount=r;
         colCount=c;
@@ -42,7 +33,6 @@ public class level extends ApplicationAdapter {
                 level1[j][b] = 'f';
             }
         }
-
     }
 
     public char[][] getLevel(){
@@ -75,64 +65,4 @@ public class level extends ApplicationAdapter {
                 level1[i][c]=fill;
         }
     }
-
-    @Override
-    public void create() {
-        spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-        bucketTexture = new Texture("bucket.png");
-        dropTexture = new Texture("drop.png");
-        //char[][] level1 = new char[5][5];
-
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-
-        batch.begin();
-        //batch.draw(image, 140, 210);
-        draw();
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
-
-    public void resize(int width, int height) {
-        viewport.update(width, height, true);
-    }
-
-    private void draw() {
-
-        viewport.apply();
-        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-        //spriteBatch.begin();
-        //while (run) {
-        //if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-        //ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        //ScreenUtils.clear(Color.BLACK);
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (level1[i][j] == 'w') {
-                    batch.draw(bucketTexture, i * 20, j * 20, 20, 20);
-                }
-                if (level1[i][j] == 'f') {
-                    batch.draw(dropTexture, i * 20, j * 20, 20, 20);
-                }
-                //System.out.print("");
-            }
-            //count++;
-        }
-        // if (count > 5)
-        //   run = false;
-        //   }
-    }
-    //batch.end();
-    //}
 }
