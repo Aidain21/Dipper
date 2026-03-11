ce40a0675f8768ab11e87351b931fe60069318b7
Alex Dabrowski <dabrowa@sunypoly.edu>
2026-03-11 11:22:26 -0400

    empty levels now show as the void texture on the minimap

--- core/src/main/java/io/github/some_example_name/MiniMap.java
+++ core/src/main/java/io/github/some_example_name/MiniMap.java
@@ -4,18 +4,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-import static io.github.some_example_name.LevelDraw.backgroundTexture;
-import static io.github.some_example_name.LevelDraw.portalTexture;
+import java.util.Arrays;
+
+import static io.github.some_example_name.LevelDraw.*;
 
 public class MiniMap {
 
     public static void drawMap(SpriteBatch batch, map map, level level){
         level[][] m=map.getMap();
+        level temp = new level(0,0);
         for (int j = 0; j < m.length; j++) {
             for (int i = 0; i < m[0].length; i++) {
                 //shows the current level
                 if(m[j][i].equals(level))
                     batch.draw(portalTexture,800+(i*32),650+(j*32),32, 32);
+                //empty levels
+                else if(Arrays.deepEquals(m[j][i].getLevel(), temp.getLevel()))
+                    batch.draw(voidTx,800+(i*32),650+(j*32),32, 32);
                 //shows the rest of the levels
                 else
                     batch.draw(backgroundTexture,800+(i*32),650+(j*32),32, 32);
