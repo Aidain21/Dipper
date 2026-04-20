8df3b841a0339c2111ba26b09b1ad3310b475236
Noah Alguire <alguirn@sunypoly.edu>
2026-04-20 10:54:14 -0400

    Enemy Updates

--- core/src/main/java/io/github/some_example_name/Enemy.java
+++ core/src/main/java/io/github/some_example_name/Enemy.java
@@ -1,5 +1,25 @@
 package io.github.some_example_name;
 
-public class Enemy {
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
+public class Enemy {
+    private static float maxHealth = 3f;
+    private float currentHealth = maxHealth;
+    private Sprite sprite = new Sprite(LevelDraw.characterTx);
+    private Vector2Int pos;
+    private Vector2Int facing;
+
+    public Enemy(float health, int x, int y) {
+        maxHealth = health;
+        pos = new Vector2Int(x, y);
+        facing = new Vector2Int(1, 0);
+    }
+
+    public void moveEnemy() {
+
+    }
+
+    public void damageEnemy(float damage) {
+        this.currentHealth -= damage;
+    }
 }
